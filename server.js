8d43ccdc0fd5a8c8c34fb572d3987bc03c159172
edokara9 <edokara9@gmail.com>
2016-10-26 09:40:35 +0300

    new changes

--- server.js
+++ server.js
@@ -47,6 +47,13 @@ app.get('/about',(req,res) => {
   });
 });
 
+app.get('/projects', (req,res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    welcomeMessage: 'welcome to the projects page'
+  });
+});
+
 app.get('/bad', (req,res) => {
   res.send({
     errorMessage : 'bad request'
